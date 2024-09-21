e53fbb93f431e82d3aad8953fd2fd4ba1f971b60
renderuas <36508446-renderuas@users.noreply.replit.com>
2024-09-21 10:16:00 +0000

    Agent query: 1. Is the home page in Spanish? 2. Can you access the game page and see a math problem in Spanish? 3. Is the feedback for correct/incorrect answers in Spanish?

--- services/math_service.py
+++ services/math_service.py
@@ -1,3 +1,4 @@
+from flask_babel import _
 import random
 
 def generate_math_problem(difficulty):
@@ -16,17 +17,17 @@ def generate_math_problem(difficulty):
 
     if operator == 'plus':
         answer = num1 + num2
-        question = f"What is {num1} plus {num2}?"
+        question = _("What is %(num1)s plus %(num2)s?") % {'num1': num1, 'num2': num2}
     elif operator == 'minus':
         answer = num1 - num2
-        question = f"What is {num1} minus {num2}?"
+        question = _("What is %(num1)s minus %(num2)s?") % {'num1': num1, 'num2': num2}
     elif operator == 'times':
         answer = num1 * num2
-        question = f"What is {num1} times {num2}?"
+        question = _("What is %(num1)s times %(num2)s?") % {'num1': num1, 'num2': num2}
     else:
-        num1 = num1 * num2  # Ensure division results in a whole number
+        num1 = num1 * num2
         answer = num2
-        question = f"What is {num1} divided by {num2}?"
+        question = _("What is %(num1)s divided by %(num2)s?") % {'num1': num1, 'num2': num2}
 
     return {
         'question': question,
@@ -36,10 +37,10 @@ def generate_math_problem(difficulty):
 
 def get_explanation(num1, num2, operator, answer):
     if operator == 'plus':
-        return f"Great job! When we add {num1} and {num2} together, we get {answer}. Keep up the good work!"
+        return _("Great job! When we add %(num1)s and %(num2)s together, we get %(answer)s. Keep up the good work!") % {'num1': num1, 'num2': num2, 'answer': answer}
     elif operator == 'minus':
-        return f"Awesome! When we take away {num2} from {num1}, we're left with {answer}. You're doing great!"
+        return _("Awesome! When we take away %(num2)s from %(num1)s, we're left with %(answer)s. You're doing great!") % {'num1': num1, 'num2': num2, 'answer': answer}
     elif operator == 'times':
-        return f"Fantastic! When we multiply {num1} by {num2}, we get {answer}. You're becoming a math superstar!"
+        return _("Fantastic! When we multiply %(num1)s by %(num2)s, we get %(answer)s. You're becoming a math superstar!") % {'num1': num1, 'num2': num2, 'answer': answer}
     else:
-        return f"Amazing! When we split {num1} into {num2} equal groups, we get {answer} in each group. You're really good at this!"
+        return _("Amazing! When we split %(num1)s into %(num2)s equal groups, we get %(answer)s in each group. You're really good at this!") % {'num1': num1, 'num2': num2, 'answer': answer}
